009f4611679928692c211ec4ace9ac7f8b9232d0
Ozan Erel <ozanerell@hotmail.com>
2025-04-01 20:55:42 +0300

    Add project files.

---
+++ MVC_AboutMe/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_AboutMe.Models;
+
+namespace MVC_AboutMe.Controllers
+{
+    public class HomeController : Controller
+    {
+        static AboutModel about = new AboutModel()
+        {
+            Name = "Ozan Erel",
+            Bio = "Merhaba,ben Okan Üniversitesi,Bilişim Sistemleri ve Teknolojileri 4.sınıf öğrencisiyim. Şu an BilgeAdam Akademi'de FullStack programlama öğrencisiyim.",
+            Hobbies = new string[] {"Kitap okumak", "Spor yapmak",  "Yeni Teknolojileri Takip Etmek" , "Yeni Şeyler Öğrenmek" }
+        };
+
+        public IActionResult Index()
+        {
+
+            return View(about);
+        }
+    }
+}
---
+++ MVC_AboutMe/Models/AboutModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_AboutMe.Models
+{
+    public class AboutModel
+    {
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string[] Hobbies { get; set; }
+    }
+}
